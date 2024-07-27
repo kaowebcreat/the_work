793e091c5fdb9886ca457f872563d89b67fb6a69
谷口夏央 <taniguchikao@taniguchinatsuos-MacBook-Air.local>
2024-07-27 19:14:39 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ $(function () {
       dots: true,
     });
   });
+
+  $(document).ready(function() {
+    function checkFadeIn() {
+        $('.fade-in-section').each(function() {
+            var elementTop = $(this).offset().top;
+            var viewportBottom = $(window).scrollTop() + $(window).height();
+
+            if (elementTop < viewportBottom - 100) { // 100px earlier than element comes into view
+                $(this).addClass('is-visible');
+            }
+        });
+    }
+
+    $(window).on('scroll', checkFadeIn);
+    checkFadeIn(); // For initial check on page load
+});
